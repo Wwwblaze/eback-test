27d87754c2df965a813b07b7c3a059ab9267db50
ActConse02812 <02812wyl@sjtu.edu.cn>
2024-05-04 22:06:40 +0800

    Order部分：将BookDao中的修改转移到BookService中

--- src/main/java/com/example/eback/serviceimpl/BookServiceImpl.java
+++ src/main/java/com/example/eback/serviceimpl/BookServiceImpl.java
@@ -5,11 +5,14 @@ import com.example.eback.entity.Book;
 import com.example.eback.entity.OrderItem;
 import com.example.eback.entity.Shopcart;
 import com.example.eback.entity.Orders;
+import com.example.eback.repository.BookRepository;
 import com.example.eback.repository.OrderRepository;
 import com.example.eback.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +25,9 @@ public class BookServiceImpl implements BookService {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
 
     @Override
     public List<Book> getBooks() {
@@ -54,7 +60,44 @@ public class BookServiceImpl implements BookService {
 
     public boolean deleteshopcart(Shopcart shopcart){return bookDao.deleteshopcart(shopcart);}
 
-    public boolean addorder(List<OrderItem> orders){return bookDao.addorder(orders);}
+    public boolean addorder(List<OrderItem> orders){
+        for(int i = 0; i < orders.size(); ++i){
+            OrderItem orderItem = orders.get(i);
+            int bookId = orderItem.getBookid();
+            Book book = bookRepository.findBookById(bookId);
+            int inventory = book.getInventory();
+            int price = orderItem.getPrice();
+            if(inventory > 0){
+                if(price >= 0){
+                    continue;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = formatter.format(date);
+        Orders newOrder = new Orders();
+        newOrder.setTime(time);
+        newOrder.setUserid(orders.get(0).getUserid());
+        int orderid = newOrder.getId();
+        for(int i = 0; i < orders.size(); i++){
+            OrderItem tem = orders.get(i);
+            int bookid = tem.getBookid();
+            Book book = bookRepository.findBookById(bookid);
+            int inventory = book.getInventory();
+
+            book.setInventory(inventory-1);
+            tem.setOrderid(orderid);
+            tem.setTime(time);
+        }
+        return bookDao.addorder(orders);
+    }
 
     public List<Book> searchbook(String str){return bookDao.searchbook(str);}
 
